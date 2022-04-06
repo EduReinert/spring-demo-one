b2443b73dd28a324a9f91a17c9797f0eea749b0b
EduReinert <eduardo.a.reinertt@gmail.com>
2022-04-06 14:02:12 -0300

    42

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,9 +1,14 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
 
     @Override
     public String getFortune(){
-        return "Today is your lucky day!(maybe not)";
+        int[] myFortune = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        Random generator = new Random();
+        int randomIndex = generator.nextInt(myFortune.length);
+        return "Today is(not) your lucky day! Your value is: " + myFortune[randomIndex];
     }
 }
